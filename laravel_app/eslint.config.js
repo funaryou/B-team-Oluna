3e5fb10158a998f73cad6038d338c87df3524daf
chika3742 <kazu.chika.shima@gmail.com>
2025-12-15 01:50:33 +0000

    Add Vue + Inertia + Vuetify integration

---
+++ laravel_app/eslint.config.js
@@ -0,0 +1,29 @@
+import pluginVue from "eslint-plugin-vue"
+import {defineConfigWithVueTs, vueTsConfigs} from "@vue/eslint-config-typescript"
+import stylistic from "@stylistic/eslint-plugin"
+
+export default defineConfigWithVueTs(
+    pluginVue.configs["flat/essential"],
+    vueTsConfigs.recommended,
+    {
+        rules: {
+            "vue/multi-word-component-names": "off",
+        },
+    },
+    { // stylistic configs
+        plugins: {
+            "@stylistic": stylistic,
+        },
+        rules: {
+            "@stylistic/indent": ["error", 4],
+            "@stylistic/comma-dangle": ["error", "always-multiline"],
+            "@stylistic/quotes": ["error", "double"],
+        },
+    },
+    {
+        ignores: [
+            "vendor/**",
+            "public/**",
+        ],
+    },
+)
