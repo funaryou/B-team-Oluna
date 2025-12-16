4ace05ac09abf86ac881aa7c9901cdc7c4874e5a
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-12-17 00:35:29 +0900

    PostController・Postモデル・マイグレーションの型を作成

---
+++ laravel_app/app/Http/Controllers/PostController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    // 一覧表示
+    public function index()
+    {
+        //
+    }
+
+    // 詳細表示
+    public function show($id)
+    {
+        //
+    }
+
+    // 検索
+    public function search(Request $request)
+    {
+        //
+    }
+
+    // public function storeForm()
+    // {
+    //     //
+    // }
+
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
+}
