0f7fe7cf890f09f0439941b91fcaeb76b4b93e27
chika3742 <kazu.chika.shima@gmail.com>
2025-12-22 05:52:55 +0000

    ニュースリスト要素コンポーネント作成

---
+++ laravel_app/resources/js/components/NewsListItem.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface Props {
+    title: string
+    tags: string[]
+    imageUrl: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+    <v-card rounded="xl" elevation="8">
+        <v-img class="thumb-img" :src="imageUrl" />
+        <v-card-subtitle class="d-flex ga-2">
+            <span v-for="tag in tags" :key="tag">#{{ tag }}</span>
+        </v-card-subtitle>
+        <v-card-title class="text-h5">{{ title }}</v-card-title>
+    </v-card>
+</template>
+
+<style lang="scss" scoped>
+@use "vuetify/settings";
+@use "sass:map";
+
+.thumb-img {
+    $margin: 8px;
+    margin: $margin;
+    border-radius: calc(map.get(settings.$rounded, "xl") - $margin);
+}
+</style>
