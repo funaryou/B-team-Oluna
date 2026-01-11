c02b3e1d86339353cae41b9b5d0c9be94dce5bde
Chikamatsu Kazuya <43089218+chika3742@users.noreply.github.com>
2026-01-11 21:47:21 +0900

    Wayfinder の導入 (#8)

--- laravel_app/eslint.config.js
+++ laravel_app/eslint.config.js
@@ -24,6 +24,7 @@ export default defineConfigWithVueTs(
         ignores: [
             "vendor/**",
             "public/**",
+            "resources/js/{actions,routes,wayfinder}/**",
         ],
     },
 )
