e8f577670e9806e9eabedf3b811d7d698d76ba91
chika3742 <kazu.chika.shima@gmail.com>
2026-01-16 07:52:43 +0000

    ヘッダーなどの共通レイアウト作成

---
+++ laravel_app/resources/js/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import siteLogo from "@/assets/img/site_logo.png"
+import {Link} from "@inertiajs/vue3";
+</script>
+
+<template>
+<header class="container bg-nav-background">
+    <Link href="/">
+        <v-img :src="siteLogo" />
+    </Link>
+</header>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    height: 60px;
+    padding: 8px 0;
+    $rounding-radius: 30px;
+    border-bottom-right-radius: $rounding-radius;
+    border-bottom-left-radius: $rounding-radius;
+    box-shadow: 0 5px 5px rgb(#000000, 25%);
+}
+</style>
--- laravel_app/resources/js/layouts/default.vue
+++ laravel_app/resources/js/layouts/default.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import AppHeader from "@/components/AppHeader.vue";
 </script>
 
 <template>
-    <v-app>
+    <v-app class="bg-background">
+        <AppHeader />
+
         <v-main>
             <v-container>
                 <slot />
