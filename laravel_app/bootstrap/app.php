c69976903c2641273a984edd5060052d03c3b21b
wkbys <wakabayashi.m.24kdgn@gmail.com>
2026-01-16 15:36:34 +0900

    Revert "Merge remote-tracking branch 'origin/dev' into feature/mahiro/create-PostController"
    This reverts commit 71a7271d438d71fc8ac6dfa80db040054cb26e7a, reversing
    changes made to 24b4031d67a5ff985f00ceb56925f529933ad81b.

--- laravel_app/bootstrap/app.php
+++ laravel_app/bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,9 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            HandleInertiaRequests::class
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
