74a6d01ded6a51946c2bd5b63b6ef8816fc7ff4e
chika3742 <kazu.chika.shima@gmail.com>
2026-01-19 05:42:12 +0000

    ルート設定

--- laravel_app/routes/web.php
+++ laravel_app/routes/web.php
@@ -4,7 +4,26 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render("index");
+    // サンプルデータ
+    return Inertia::render("home", ["items" => [
+        [
+            "id" => 1,
+            "title" => "Testtest",
+            "text" => "Test Text",
+            "likes" => 40,
+            "thumbnail" => "",
+            "tags" => [["tags" => "test"], ["tags" => "test2"]],
+            ""
+        ],
+        [
+            "id" => 2,
+            "title" => "Testtest",
+            "text" => "Test Text2",
+            "likes" => 40,
+            "thumbnail" => "",
+            "tags" => [["tags" => "test"], ["tags" => "test2"]]
+        ],
+    ]]);
 });
 
 Route::get("/posts/{id}", function () {
