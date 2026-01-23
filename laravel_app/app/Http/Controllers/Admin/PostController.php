28a267ddb29252b81b41a2fde38375434d0b520b
wkbys <wakabayashi.m.24kdgn@gmail.com>
2026-01-23 16:54:20 +0900

    Admin/PostControllerを作成

---
+++ laravel_app/app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Content;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PostController extends Controller
+{
+    /**
+     * 新規投稿
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
