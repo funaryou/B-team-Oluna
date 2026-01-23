c411e402850cfbe6376d4edcd8e898892ba7ea07
wkbys <wakabayashi.m.24kdgn@gmail.com>
2026-01-23 17:04:01 +0900

    新規投稿用ルートを追加

--- laravel_app/routes/web.php
+++ laravel_app/routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', [PostController::class, "index"])->name("web.top");
 Route::get("/posts/{id}", [PostController::class, "show"])->name('web.posts.detail');
 
 Route::get("/search", [PostController::class, "search"])->name('web.search');
+
+Route::post('/admin/posts', [Admin\PostController::class, 'store'])->name('admin.posts.store');
