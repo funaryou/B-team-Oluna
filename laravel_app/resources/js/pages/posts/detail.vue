1ca6dcac29f11da187d0556d1ef17ed63caeb9bc
funa-dev <funahashi.ryouma@gmail.com>
2026-01-30 00:28:41 +0900

    実データとの連携と画像が表示されていなかったので、フォーマットでの画像パス作成の修正

--- laravel_app/resources/js/pages/posts/detail.vue
+++ laravel_app/resources/js/pages/posts/detail.vue
@@ -8,26 +8,12 @@ export default {
 <script setup lang="ts">
 import PostDetail from "@/components/PostDetail.vue";
 
-// デモデータ
-const demoText = Array(450).fill("TEXT").join("")
-const demoData = {
-    id: 1,
-    title: "OLUNA - デモ投稿タイトル",
-    text: "これはデモ投稿の本文です。\n\n複数行にわたるテキストコンテンツをここに表示します。\n\n実際のアプリケーションでは、データベースから取得した記事の内容がここに表示されます。" + demoText,
-    thumbnail: "https://picsum.photos/800/450?random=4",
-    tags: ["t", "te", "tes","test", "testt", "testtt","testttt", "testtttt", "testttttt","testtttttt", "testttttttt", "testtttttttt"],
-    likes: 42,
-    images: [
-        "https://picsum.photos/800/450?random=1",
-        "https://picsum.photos/800/450?random=2",
-        "https://picsum.photos/800/450?random=3"
-    ]
-}
+const { data } = defineProps<{ data: any }>()
 </script>
 
 <template>
     <div>
-        <PostDetail :data="demoData" />
+        <PostDetail :data="data" />
     </div>
 </template>
 
